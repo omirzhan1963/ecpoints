aa7a3aacb9b9a2a6618d28a5d998603bf4ced910
omirzhan1963 <abdrashev63@mail.ru>
2019-07-17 16:57:58 +0600

    forth comm

---
+++ comparelists.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 16:47:52 2019
+
+@author: Bilim
+"""
+def dfile(fname):
+    f=open(fname,"r")
+    lines=f.readlines()
+    l=[]
+    for k in lines:
+        ik=int(k)
+        l.append(ik)
+    return l
+def compl(l1,l2):
+    l=l1+l2
+    l.sort()
+    i=0
+    r=False
+    for k in l:
+        if k==i:
+            r=True
+            print("ooo")
+        i=k
+    return r
